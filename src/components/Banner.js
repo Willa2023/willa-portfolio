06f44c9185ae9dc6220d48b14f81d3a44827adee
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-08-19 20:29:33 +1200

    add banner style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ import headerImg from '../assets/img/header-img.svg';
 const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ['Software Developer', 'Web Developer', 'Mobile Developer', 'Full Stack Developer'];
+    const toRotate = ['Software Developer', 'Web Developer', 'Full Stack Developer'];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(200-Math.random()*100);
     const period = 2000;
@@ -48,7 +48,9 @@ const Banner = () => {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to Willa's Portfolio</span>
-                        <h1>{`Hi I'm Willa. `}<span className='wrap'>{text}</span></h1>
+                        <h1>{`Hi I'm Willa. `}
+                            <span className='txt-rotate'><span className='wrap'>{text}</span></span>
+                        </h1>
                         <p>
                             I am a new graduate with a passion for creating innovative solutions, looking for an opportunity to grow as a software developer. I have practical projects in web development, mobile development. I am always looking for new challenges and opportunities to grow.
                         </p>
